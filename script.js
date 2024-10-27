778d9500077ee7efc64e64a130f4d54dd794214f
mashqoor <mashqooru@gmail.com>
2024-10-28 00:13:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openPage2() {
+  // Get the page elements
+  const page1 = document.getElementById("page1");
+  const page2 = document.getElementById("page2");
+
+  // Hide the first page
+  page1.style.display = "none";
+
+  // Show the second page
+  page2.style.display = "block";
+}
